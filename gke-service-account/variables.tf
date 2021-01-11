6f4987aaee13130bed84bbe27b7bbdae1e8a0dca
huytn1219 <61076435+huytn1219@users.noreply.github.com>
2021-01-10 18:49:39 -0800

    Update variables.tf

--- gke-service-account/variables.tf
+++ gke-service-account/variables.tf
@@ -5,11 +5,13 @@
 variable "project" {
     description = "The name of the GCP Project where all resources will be launched."
     type        = string
+    default     = "summer-monument-301402"
 }
 
 variable "name" {
     description = "The name of the custom service account. This parameter is limited to a maximum of 28 characters."
     type        = string
+    default     = "test-svc
 }
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
