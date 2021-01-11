0a4cb34c5b2b5519b59e4b72d8c3eea5abab3dc8
Huy Nguyen <huynguyen@Huys-MacBook-Pro.local>
2021-01-11 08:49:09 -0800

    You're doing great

--- gke-service-account/variables.tf
+++ gke-service-account/variables.tf
@@ -11,7 +11,7 @@ variable "project" {
 variable "name" {
     description = "The name of the custom service account. This parameter is limited to a maximum of 28 characters."
     type        = string
-    default     = "test-svc-v2"
+    default     = "test-svc-v3"
 }
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
